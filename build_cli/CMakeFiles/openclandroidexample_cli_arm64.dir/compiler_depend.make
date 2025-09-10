16ba56d5c4eeac67a7c1f465a346ee4b741ef519
mtb <mtb0x10x1@gmail.com>
2025-09-10 17:08:46 +0200

    Delete build_cli directory

--- build_cli/CMakeFiles/openclandroidexample_cli_arm64.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for openclandroidexample_cli_arm64.
-# This may be replaced when dependencies are built.
--- build_cli/CMakeFiles/openclandroidexample_cli_arm64.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for openclandroidexample_cli_arm64.
